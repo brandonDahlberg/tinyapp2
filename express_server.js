0568fb5d2d472be5262e6bfd3e023d21ccf9f4d9
brandonDahlberg <brandon.dahlberg21@gmail.com>
2022-01-29 18:47:37 +0000

    updated security features

--- express_server.js
+++ express_server.js
@@ -12,8 +12,8 @@ app.use(cookieParser());
 
 // URL-DATABASE
 const urlDatabase = {
-	b2xVn2: 'http://www.lighthouselabs.ca',
-	'9sm5xK': 'http://www.google.com',
+	b2xVn2: { longURL: 'http://www.lighthouselabs.ca', userID: 'userRandomID' },
+	'9sm5xK': { longURL: 'http://www.google.com', userID: 'user2RandomID' },
 };
 
 // USERS DATABASE
@@ -46,8 +46,21 @@ const verifyUserEmail = function (email) {
 	for (let id in users) {
 		if (users[id].email === email) return users[id];
 	}
-	return null;
+	return false;
 };
+
+//
+// POST REQUESTS
+//
+
+/* UPDATE DATABASE AND HOME PAGE WITH { SHORT-URL : LONG-URL }
+  FROM CREATE URL PAGE */
+app.post('/home', (req, res) => {
+	const longURL = req.body.longURL;
+	const shortURL = rndmStr();
+	urlDatabase[shortURL] = { longURL, userID: req.cookies['user_id'] };
+	res.redirect('/home');
+});
 
 // Register POST
 app.post('/register', (req, res) => {
@@ -59,67 +72,43 @@ app.post('/register', (req, res) => {
 		res.status(403).send('user already exists');
 	} else if (!userObj) {
 		users[newID] = { id: newID, email: newEmail, password: newPassword };
-		res.redirect('/login');
+		res.cookie('user_id', users[newID].id);
+		res.redirect('/home');
 	}
+
+	// Login POST
+	app.post('/login', (req, res) => {
+		const candidateEmail = req.body.email;
+		const candidatePassword = req.body.password;
+		const userObj = verifyUser(candidateEmail, candidatePassword);
+		if (userObj) {
+			res.cookie('user_id', userObj.id);
+			res.redirect('/home');
+		} else res.send('Login Invalid');
+	});
 });
+
+// Login GET
+app.get('/login', (req, res) => {
+	const tempVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
+	res.render('login', tempVars);
+});
+
+// Logout POST
+app.post('/logout', (req, res) => {
+	res.clearCookie('user_id');
+	res.redirect('/login');
+});
+
 // Register GET
 app.get('/register', (req, res) => {
 	const tempVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
 	res.render('register', tempVars);
 });
 
-// Login POST
-app.post('/login', (req, res) => {
-	const candidateEmail = req.body.email;
-	const candidatePassword = req.body.password;
-	const userObj = verifyUser(candidateEmail, candidatePassword);
-	if (userObj) {
-		res.cookie('user_id', userObj.id);
-		res.redirect('/home');
-	} else res.status(403).send('Login Invalid');
-});
-
-// Login GET
-app.get('/login', (req, res) => {
-	const tempVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
-	console.log(tempVars);
-	res.render('login', tempVars);
-});
-// Logout POST
-app.post('/logout', (req, res) => {
-	res.clearCookie('user_id');
-	res.redirect('/login');
-	console.log(users);
-});
-/* UPDATE DATABASE AND HOME PAGE WITH { SHORT-URL : LONG-URL } 
-    FROM CREATE URL PAGE */
-app.post('/home', (req, res) => {
-	const longURL = req.body.longURL;
-	const shortURL = rndmStr();
-	urlDatabase[shortURL] = longURL;
-	res.redirect(`/home`);
-});
-
-// HANDLES POST REQUESTS FROM EDIT FORM ON SHOW PAGE
-app.post('/home/:shortURL', (req, res) => {
-	const longURL = req.body.longURL;
-	const shortURL = req.params.shortURL;
-	const tempVars = { longURL: longURL, shortURL: shortURL };
-	urlDatabase[shortURL] = longURL;
-	res.render('show', tempVars);
-});
-
-// DELETE URLS
-app.post('/home/:shortURL/delete', (req, res) => {
-	const shortURL = req.params.shortURL;
-	delete urlDatabase[shortURL];
-	res.redirect('/home');
-});
-
-// RENDER HOME PAGE
+// RENDER HOME PAGE GET
 app.get('/home', (req, res) => {
 	const tempVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
-
 	res.render('home', tempVars);
 });
 
@@ -128,28 +117,71 @@ app.get('/home/createURL', (req, res) => {
 	const tempVars = {
 		user: users[req.cookies['user_id']],
 	};
-	res.render('createURL', tempVars);
-});
-// RENDER SHOW SHORTURL PAGE
-app.get('/home/:shortURL', (req, res) => {
-	const url = urlDatabase[req.params.shortURL];
-	const tempVars = {
-		shortURL: req.params.shortURL,
-		longURL: urlDatabase[req.params.shortURL],
-		user: users[req.cookies['user_id']],
-	};
-	res.render('show', tempVars);
+	if (req.cookies['user_id']) {
+		res.render('createURL', tempVars);
+	} else {
+		res.redirect('/login');
+	}
 });
 
 // REDIRECTS BROWSER TO WEBPAGE OF LONG-URL BASED ON SHORT-URL
 app.get('/u/:shortURL', (req, res) => {
 	const shortURL = req.params.shortURL;
-	const longURL = urlDatabase[shortURL];
-	if (longURL) {
-		res.redirect(longURL);
-	} else if (!longURL) {
+	const longURLObj = urlDatabase[shortURL];
+	if (longURLObj) {
+		res.redirect(longURLObj.longURL);
+	} else if (!longURLObj) {
 		res.status(404).send('Page Not Found');
 	}
+});
+
+// SUBMITS NEW URL ON EDIT PAGE
+app.post('/home/:shortURL', (req, res) => {
+	const longURL = req.body.longURL;
+	const shortURL = req.params.shortURL;
+	urlDatabase[shortURL].longURL = longURL;
+	const tempVars = { user: users[req.cookies['user_id']], urls: urlDatabase, longURL: longURL, shortURL: shortURL };
+	res.render('home', tempVars);
+});
+
+// EDIT BUTTON ON HOME PAGE
+app.get('/home/:shortURL', (req, res) => {
+	const shortURL = req.params.shortURL;
+	const cookieID = req.cookies['user_id'];
+	const userID = urlDatabase[shortURL].userID;
+	const tempVars = {
+		shortURL: req.params.shortURL,
+		longURL: urlDatabase[shortURL],
+		user: users[req.cookies['user_id']],
+	};
+	if (cookieID) {
+		// make sure user is logged in and the urls belong to him.
+		if (userID && cookieID === userID) {
+			res.render('show', tempVars);
+		} else {
+			res.redirect('/error');
+		}
+	}
+});
+
+// DELETE URLS
+app.post('/home/:shortURL/delete', (req, res) => {
+	const shortURL = req.params.shortURL;
+	const cookieID = req.cookies['user_id'];
+	const userID = urlDatabase[shortURL].userID;
+	if (cookieID) {
+		// make sure user is logged in and the urls belong to him.
+		if (userID && cookieID === userID) {
+			delete urlDatabase[shortURL];
+			res.redirect('/home');
+		} else {
+			res.redirect('/error');
+		}
+	}
+});
+
+app.get('/error', (req, res) => {
+	res.render('error');
 });
 
 app.listen(PORT, () => {
